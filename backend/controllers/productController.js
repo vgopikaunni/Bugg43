4c691e4703453489216e2a252b31aae56f9e6fed
Gopika <rajitha5901@gmail.com>
2025-03-11 18:22:53 +0530

    backend started

---
+++ backend/controllers/productController.js
@@ -0,0 +1,24 @@
+
+
+// function for add product
+const addProduct = async (req, res) => {
+
+}
+
+
+
+// function for list product
+const listProducts = async (req, res) => {
+}
+
+
+
+// function for removing product
+const removeProduct = async (req, res) => {
+
+}
+
+// function for single product info
+const singleProduct = async (req, res) => {
+
+}
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,12 +35,15 @@ const Navbar = () => {
 
                 {/* Profile Dropdown */}
                 <div className='group relative'>
-                    <img 
-                        onClick={() => token ? null : navigate('/login')} 
-                        className='w-5 cursor-pointer' 
-                        src={assets.profile_icon} 
-                        alt="Profile"
-                    />
+                    
+                <Link to='/login'>
+  <img
+    onClick={() => (token ? null : navigate('/login'))}
+    className='w-5 cursor-pointer'
+    src={assets.profile_icon}
+    alt="Profile"
+  />
+</Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
